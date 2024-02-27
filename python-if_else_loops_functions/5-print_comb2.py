b5e6df587f28e080a4e48547a57ba58069407b35
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-02-27 19:26:12 +0200

    fixing up again

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#This script prints upto 99 with spaces in-between.
 for num in range(0, 100):
     if num == 99:
         print("{}".format(num))
--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#This script checks for lower case characters.
 def islower(c):
-    #scans the ASCII characters for the lowercase ones
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
