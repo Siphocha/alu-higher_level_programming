60a33031c1780a6fc2dbec1dcb170d62134dafe9
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-02-27 20:25:19 +0200

    fixes

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        temp = char
-        if ord(temp) >= 97 and ord(temp) <= 122:
-            temp = chr(ord(char) - 32)
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
         print("{}".format(char), end="")
-    print()
+    print("")
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def present_last_digit(num):
-    if num < 0:
-        num = -num
-    present_last_digit = num % 10
-    print("{}".format(present_last_digit), end="")
-    return present_last_digit
+def last_digit(num):
+    print(abs(num) % 10, end="")
+    return (abs(num) % 10)
