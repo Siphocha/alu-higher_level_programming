ef1cfd8b9490af3386b5dc0f77ef52257578a2fa
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-15 23:18:28 +0200

    try to fix this

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Defining a class known as Rectangle"""
+# 1-rectangle.py
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    """Initialising rectangle and definnning it"""
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Call property function to use attributes defined above"""
+        """Get/set the width of the rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -21,12 +31,13 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
