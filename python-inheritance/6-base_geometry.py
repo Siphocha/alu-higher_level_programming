b8bb2bd7e4a04edd560e4dca1f6fc241e0ec44c8
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-16 23:35:58 +0200

    trying again

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -2,7 +2,7 @@
 """Defining attribute of first class"""
 
 class BaseGeometry:
-
+    """Class with base attribute"""
     def area(self):
         """function for defining area"""
         raise Exception("area() is not implemented")
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
+# 9-rectangle.py
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-"""This class Rectangle is a subclass of BaseGeometry"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Child class using parent attributes"""
+    """Represent a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
-        """initlaising new rectangle shape adding variables"""
-        self.integer_validator("width", width)
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height 
+        super().integer_validator("height", height)
+        self.__height = height
 
     def area(self):
-        """Gets area of rectangle"""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
-    
+
     def __str__(self):
-        """returns print() rep of rectangle shape"""
-        string = "[" + str(self.__class__.___name___) + "] "
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
-
-
