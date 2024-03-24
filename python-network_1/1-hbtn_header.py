f2a03ca6ae1c2b7f978dcc3c6beb5bcf615065f9
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-24 16:40:49 +0200

    Making all files executable soon just not now doing something elese

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """Takes URL (post) sends request back to display value "x-request-id"""
 import sys
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with request.urlopen(sys.argv[1]) as res:
+        print(res.info()["X-Request-Id"])
