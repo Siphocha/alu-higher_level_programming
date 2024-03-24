4130b53046b4d6b7c48ae314ca5b12e3c1fcdd2e
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-24 16:37:18 +0200

    This is exhausting

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Takes URL (post) sends request back to display value "x-request-id"""
 import sys
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as res:
-        print(res.info()["X-Request-Id"])
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
