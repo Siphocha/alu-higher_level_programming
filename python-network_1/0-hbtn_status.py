08178192e61222f91b8acac332e5077575f48afb
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-24 13:43:03 +0200

    lets try this again AM CRYING SO MUCH

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,12 +2,21 @@
 """This script fetches "https://alu-intranet.hbtn.io/status
    have to use urlib module/library"""
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
 
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+
+    # Create a request object
+    with urllib.request.urlopen(url) as response:
+        # Read the response content
+        content = response.read()
+
+        # Decode the content to UTF-8
+        utf8_content = content.decode('utf-8')
+
+        # Display response
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(utf8_content))
