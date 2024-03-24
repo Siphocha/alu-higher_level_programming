751a8b9203b8a0d9a74a701670375afafedcb2a4
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-03-24 22:49:39 +0200

    LETS TRY AGAIN

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,16 +2,16 @@
 """This script fetches "https://alu-intranet.hbtn.io/status
    have to use urlib module/library"""
 import urllib.request
+
 url = 'https://intranet.hbtn.io/status'
 if url.startswith('https://'):
     url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-
-    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as res:
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
 
