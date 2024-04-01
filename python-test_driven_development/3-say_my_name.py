97a28d03aab8d6eb3997af4b772f70b3bdd3e385
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-01 16:14:07 +0200

    geeez its gotten from worse to worser

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""Module for say_my_name method."""
+"""Function for saying your name outloud called say_my_name"""
 
+def say_my_name(first_name, last_name=" "):
+    """functionfor printing first-and-last name"""
 
-def say_my_name(first_name, last_name=""):
-    """Method for printing first and last name.
-
-    Args:
-        first_name: first name string.
-        last_name: last name string.
-
-    Raises:
-        TypeError: If first_name or last_name are not strings.
-    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
+    
+    """printing both the names being printed out"""
     print("My name is {:s} {:s}".format(first_name, last_name))
 
 if __name__ == "__main__":
