4bc7f0905cbeba7f08f73b8e96e0e0a953d2f38f
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-01 14:12:17 +0200

    ahhhhh here we go again

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """making a function for adding integers
 Call that add_integer"""
-import doctest
 
 def add_integer(a, b=98):
     """This is the function for adding"""
@@ -12,5 +11,6 @@ def add_integer(a, b=98):
     return int(a) + int(b)
 
 if __name__ == "__main__":
+    import doctest
     doctest.testfile("tests/0-add_integer.txt")
     
