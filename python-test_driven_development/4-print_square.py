a848682a21e55c53178c0b246070855dae4318d2
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-01 16:38:26 +0200

    helpppp

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Function for printing suqare with #"""
+"""Module for print_square method."""
+
 
 def print_square(size):
-    """Function for printing #-square shape"""
+    """Method for printing a square with # characters.
+
+    Args:
+        size: The int size of the square's side.
+
+    Raises:
+        TypeError: If size is not an int.
+        ValueError: If size is < 0.
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    
-    """printing square with hashtags"""
+
     print((("#" * size + "\n") * size), end="")
 
 if __name__ == "__main__":
