fed3db22d993c01a2f26de3056deceddd84fdf3e
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-02 21:45:17 +0200

    wahhhhhhh

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""This function prints two new lines after one of the characters . ? :"""
-def text_identation(text):
-    """Actual function for putting the lines after . ? :"""
+"""Module for text_indentation method."""
+
+
+def text_indentation(text):
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    for jmp in ".?:":
-        txt = (jmp + "\n\n").join([line.strip(" ") for line in text.split(jmp)])
-        print(txt, end="")
+
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("tests/5-text_identation.txt")
+    doctest.testfile("tests/5-text_indentation.txt")
