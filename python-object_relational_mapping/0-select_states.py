7f841a5e203558c5bd6eb6701aeec296cabe797f
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-04 17:36:24 +0200

    i was right was just taking a while to confirm me...rude

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-Script to list all states from the database 'hbtn_0e_0_usa'
+"""Script listing all states from hbtn_0e_0_usa
+This script connects to MYSQL server on localhost on a port.
+retrieves all rows from states sorted in ASCENDING order"""
 
-This script connects to a MySQL server running on 'localhost' at port '3306'
-and retrieves all rows from the 'states' table in the 'hbtn_0e_0_usa' database.
-The results are sorted in ascending order by the 'id' column and displayed on
-the console.
-
-Usage:
-    ./script.py <mysql_username> <mysql_password> <database_name>
-
-Arguments:
-    mysql_username: The username to connect to the MySQL server.
-    mysql_password: The password associated with the MySQL username.
-    database_name: The name of the MySQL database to connect to.
-"""
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     username, password, db_name = sys.argv[1:4]
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
+    """We use the sys because database stored locally"""
+    db = MySQLdb.connect(host="localhost", port=3306,  user=username, passwd=password, db=db_name)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+    """fetching from the database using SELECT and fetchall again."""
     results = cur.fetchall()
     for row in results:
         print(row)
