74f6b6cc61e833e9ed833efa148e0e989008de37
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-04 16:55:14 +0200

    lets see if thislll work I doubt it though

---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,6 @@
+In this project we will be continuing with MYSQL....BUT IN A COOL WAY!
+
+We will continue with Databases but with connecting with Python.
+Specifcially using SQLAlchemy Thats right! this is for:
+
+Object-Relational-Mapping reasons! (ORM) let's see how much more smooth it is!
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""Module for text_indentation method."""
-
-
-def text_indentation(text):
-    """Method for adding 2 new lines after '.?:' chars.
-
-    Args:
-        text: The str text.
-
-    Raises:
-        TypeError: If text is not a str.
-    """
+"""This function prints two new lines after one of the characters . ? :"""
+def text_identation(text):
+    """Actual function for putting the lines after . ? :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    for delim in ".?:":
-        # print(delim, text.split(delim))
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
-
-    print(text, end="")
+    
+    for jmp in ".?:":
+        txt = (jmp + "\n\n").join([line.strip(" ") for line in text.split(jmp)])
+        print(txt, end="")
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    doctest.testfile("tests/5-text_identation.txt")
