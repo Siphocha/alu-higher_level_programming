1fe2910cdce386e5ae7b744f9f81cf514c760df5
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-06 12:41:05 +0200

    Have a theory but unsure lets see

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""prints State object specfiied by name argument"""
+"""prints State object specfiied by
+ name argument"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
