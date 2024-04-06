3249aa143d4c651cd770293d9a7df32ced26ed4c
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-06 16:05:42 +0200

    make that change (says in michael jackson voice)

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Changes the name of State Object"""
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa
+"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    """once session is activated we get the new State name from by activating query"""
-    state = session.query(State).filter_by(id=2).first()
-    state.name = 'New Mexico'
-    session.commit()
-    session.close()
+
+    stateUpdated = session.query(State).filter(State.id == 2).first()
+
+    if stateUpdated:
+        stateUpdated.name = 'New Mexico'
+        session.commit()
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""This prints first State object"""
+"""
+Prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first = session.query(State).order_by(State.id).first()
-    """Pythonic way of calling a sessionn query to then get first states object"""
-    if first:
-        print("{}: {}".format(first.id, first.name))
-    else:
-        print("Nothing")
-    session.close()
+
+    state = session.query(State).order_by(State.id).first()
+
+    print("Nothing" if not state else "{}: {}".format(state.id, state.name))
