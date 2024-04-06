827bac677a4a1b40838af93ed6d431a80e4ad317
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-06 15:32:44 +0200

    dang this is getting harder

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""script lists all cities from daatabase including in all the states."""
+"""
+List all cities from a database
+"""
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \ JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
