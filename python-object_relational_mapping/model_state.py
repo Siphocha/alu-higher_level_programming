244b8467e5bddd9e58b0d6ebf39bf24b09f47a67
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-06 15:44:53 +0200

    Will this work?

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""a link class to table for the database"""
+"""
+a link class to table for the database
+"""
+
 import sys
 from model_state import Base, State 
 from sqlalchemy import(create_engine)
