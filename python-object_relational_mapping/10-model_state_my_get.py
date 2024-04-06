15366fdcc2da17e647904aaf7eb78e9efff719bd
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-06 15:27:23 +0200

    lets see if putting my comments like this would work

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Return State object matching arg"""
-
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+"""
+prints State object specfiied by name argument
+"""
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).\
-        filter(State.name == argv[4]).order_by(State.id).all()
-    if states:
-        print("{}".format(states[0].id))
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+    if state:
+        print("{}".format(state.id))
     else:
         print("Not found")
     session.close()
