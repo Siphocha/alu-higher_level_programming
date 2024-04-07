963e0ba7eeaed6f6ccfa0a57eb12a141124a86f4
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-04-07 13:38:17 +0200

    lets see if things will work all at once here

---
+++ python-almost_a_circle/5-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 5-main Input for rectangle but with more integers overlaying x&y coords"""
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6, 2, 1, 12)
+    print(r1)
+
+    r2 = Rectangle(5, 5, 1)
+    print(r2)
