3b26321a5cb7ebebf8540fed06922498ff8aa7d5
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-06-04 15:20:32 +0200

    here we go again

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// This script argues for and prints factorial numbers.
-function factorial (x) {
-  if (isNaN(x) || x < 0) return 1;
-  if (x === 0) return 1;
-  return x * factorial(x- 1);
+//This script argues for and prints factorial numbers.
+function factorial (n) {
+  if (isNaN(n) || n < 0) return 1;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
