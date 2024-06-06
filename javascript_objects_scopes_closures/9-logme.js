fbd0302e4ee2272c8647badf4b842ec440274a22
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-06-07 01:39:42 +0200

    shiet bro this sucs am trying here crap

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/mode
 // prints number of arguments already printed and new argument
-let counting = 0;
+let counter = 0;
 exports.logMe = function (item) {
-  console.log(`${counting}: ${item}`);
-  counting++;
+  console.log(`${counter}: ${item}`);
+  counter++;
 };
