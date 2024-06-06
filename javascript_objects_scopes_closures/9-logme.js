dbd8d5c5ddf6296d45681d2c0f8f710487dc2bbc
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-06-07 01:37:13 +0200

    functions were pretty fun tbh but ONLY C CONQUERS ALLLLLLLL ONLY CCCCCCC YOU SEE THE TRUTTHTHHTHTH!

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/mode
+// prints number of arguments already printed and new argument
+let counting = 0;
+exports.logMe = function (item) {
+  console.log(`${counting}: ${item}`);
+  counting++;
+};
