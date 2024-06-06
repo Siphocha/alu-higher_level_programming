357becdb3c65b0253044ccdf6da999acc2287126
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-06-07 01:41:24 +0200

    ITS 2 AM AND WHAT KEPT ME UP WAS A MISPELL I HATE YOU WHOEVER READS THIS I WISH YOU NOTHING BUT THE WORST DIEEEEE

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
-#!/usr/bin/mode
+#!/usr/bin/node
 // prints number of arguments already printed and new argument
-let counter = 0;
+let counting = 0;
 exports.logMe = function (item) {
-  console.log(`${counter}: ${item}`);
-  counter++;
+  console.log(`${counting}: ${item}`);
+  counting++;
 };
